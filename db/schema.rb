a533c356ef64936f84a56c2d52ad43e5f2eb0bea
Ben Manning <bmanning@me.com>
2015-05-13 12:49:53 -0700

    adjustments for rspec

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 0) do
-=======
 ActiveRecord::Schema.define(version: 20150512233542) do
 
   # These are extensions that must be enabled in order to support this database
@@ -44,6 +41,6 @@ ActiveRecord::Schema.define(version: 20150512233542) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
->>>>>>> development
+
 
 end
--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 RSpec.describe WelcomeController, :type => :controller do
 
+	describe "Get #index" do
+    it "should render the :index view" do 
+      get :index
+      expect(response).to render_template(:index)
+    end
+
 end
